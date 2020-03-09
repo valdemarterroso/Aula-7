182e0c59ca1a954048dbe9a0761a3a1147c71f2e
Valdemar <valdemarterroso.esaf@gmail.com>
2020-03-09 10:09:47 -0700

    obj adicionado

--- objects.php
+++ objects.php
@@ -25,6 +25,7 @@
         function getPrice(){
             return $this->price;
         }
+
     }
 
     //Instance a book object
@@ -35,12 +36,30 @@
     $harrypotter->price = 20.79;*/
 
     $livro1 = new Book("Harry Potter", "J.K. Rowling", 20.79);
-    $livro2 = new Book("Biblia", "Deus", 99999);
-    $livro3 = new Book("Gang", " Bang", 420);
-    $livro4 = new Book("Olah", "John", 37);
+    $livro2 = new Book("Biblia", "Deus", 100);
+    $livro3 = new Book("Gang", " Bang", 17);
+    $livro4 = new Book("Olah", "John", 10);
 
     $carrinhoCompras = array($livro1, $livro2, $livro3, $livro4);
-    
+
+    //Calcular o total
+    function getTotal($carrinhoCompras){
+        $total = 0;
+        foreach($carrinhoCompras as $livro){
+            $total += $livro->getPrice();
+        }
+        return $total;
+    }
+
+    function titulosDesconto($carrinhoCompras){
+        $titulo = arra();
+        foreach($carrinhoCompras as livro){
+            if($livro->getPrice() >20){
+                array_push($titulos, $livro->getTitle());
+            }
+        }
+        return $titulos;
+    } 
 ?>
 <!DOCTYPE html>
 <html>
@@ -58,7 +77,10 @@
         <strong><?php echo $livro->getPrice(); ?> </strong>
     <?php } ?>
 
-    <h1><?php //TODO ?></h1>
+    <h2>TOTAL: <?php echo getTotal($carrinhoCompras); ?>€</h2>
+    <h2>LIVROS COM DESCONTO: <?php 
+        foreach(titulosDesconto($carrinhoCompras) as $titulos)
+    ?></h2>
 
 </body>    
 </html>
